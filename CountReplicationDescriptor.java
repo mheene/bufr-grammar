8149de63c8858fa3c94a8faf0055b7c6d771a46a
Markus Heene <markus.heene@gmail.com>
2019-11-02 20:15:54 +0100

    Counter for nested replications implemented

--- CountReplicationDescriptor.java
+++ CountReplicationDescriptor.java
@@ -1,9 +1,56 @@
-import org.antlr.v4.runtime.tree.TerminalNode;
+
+//import org.antlr.v4.runtime.tree.TerminalNode;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class CountReplicationDescriptor extends BUFRBaseListener {
     private int counterDescriptors = 0;
     private int numberOfFixedReplications = 0;
     private int numberOfDelayedReplications = 0;
+
+    private ArrayList<ReplicationDescriptor> descriptorList = new ArrayList<ReplicationDescriptor>();
+
+    private void addFixedReplicationDescriptorToList(String name, int replications) {
+        Iterator<ReplicationDescriptor> it = descriptorList.iterator();
+        while(it.hasNext()) {
+            ReplicationDescriptor rd = it.next();
+            rd.reduceReplications();
+            if (rd.getReplications() == 0) {
+                it.remove();
+            }
+        }
+        int size = descriptorList.size();
+        ReplicationDescriptor p_descriptor = new ReplicationDescriptor(size, name, "fixed", replications);
+        descriptorList.add(p_descriptor);
+    }
+
+    private void addDelayedReplicationDescriptorToList(String name, int replications) {
+       
+        Iterator<ReplicationDescriptor> it = descriptorList.iterator();
+        while(it.hasNext()) {
+            ReplicationDescriptor rd = it.next();
+            rd.reduceReplications(2);
+            if (rd.getReplications() == 0) {
+                it.remove();
+            }
+        }
+
+        int size = descriptorList.size();
+        ReplicationDescriptor p_descriptor = new ReplicationDescriptor(size, name, "delayed", replications);
+        descriptorList.add(p_descriptor);
+    }
+
+    private void processDescriptor() {
+        Iterator<ReplicationDescriptor> it = descriptorList.iterator();
+
+        while(it.hasNext()) {
+            ReplicationDescriptor rd = it.next();
+            rd.reduceReplications();
+            if (rd.getReplications() == 0) {
+                it.remove();
+            }
+        }
+    }
 
     @Override
     public void enterFixed_replication_descriptor(BUFRParser.Fixed_replication_descriptorContext ctx) {
@@ -13,6 +60,7 @@ public class CountReplicationDescriptor extends BUFRBaseListener {
         // System.out.println("" + parts.length + " x:" + parts[1]);
         System.out.println("Replications: " + Integer.parseInt(parts[1]));
         numberOfFixedReplications = Integer.parseInt(parts[1]);
+        addFixedReplicationDescriptorToList(fixReplication, numberOfFixedReplications);
         // System.out.println("x_all: " + ctx.x_all().toString());
     }
 
@@ -24,21 +72,25 @@ public class CountReplicationDescriptor extends BUFRBaseListener {
             numberOfFixedReplications--;
         }
 
-        if (numberOfDelayedReplications > 0 ) {
+        if (numberOfDelayedReplications > 0) {
             numberOfDelayedReplications--;
         }
+        this.processDescriptor();
     }
 
     @Override
     public void enterOperator_descriptor_expr(BUFRParser.Operator_descriptor_exprContext ctx) {
         System.out.println("Operator Descriptor Expr Ctx: " + ctx.getText());
         counterDescriptors++;
+        // @ToDo: konnte mehr als einer sein
+        this.processDescriptor();
     }
 
     @Override
     public void enterSequence_descriptor(BUFRParser.Sequence_descriptorContext ctx) {
         System.out.println("Sequence Descriptor Ctx: " + ctx.getText());
         counterDescriptors++;
+        this.processDescriptor();
     }
 
     @Override
@@ -54,6 +106,7 @@ public class CountReplicationDescriptor extends BUFRBaseListener {
         // System.out.println("" + parts.length + " x:" + parts[1]);
         System.out.println("Replications: " + Integer.parseInt(parts[1]));
         numberOfDelayedReplications = Integer.parseInt(parts[1]);
+        addDelayedReplicationDescriptorToList(delayedReplication, numberOfDelayedReplications);
     }
 
     @Override
@@ -61,6 +114,11 @@ public class CountReplicationDescriptor extends BUFRBaseListener {
         System.out.println("Number of FixedReplications: " + numberOfFixedReplications);
         System.out.println("Number of DelayedReplications: " + numberOfDelayedReplications);
         System.out.println("Counter of Descriptors: " + counterDescriptors);
+        System.out.println("DescriptorList");
+        Iterator<ReplicationDescriptor> it = descriptorList.iterator();
+        while(it.hasNext()) {
+            System.out.println(it.next().toString());
+        }
     }
 
     /*
@@ -68,4 +126,44 @@ public class CountReplicationDescriptor extends BUFRBaseListener {
      * System.out.println("TN counter: " + counterDescriptors);
      * System.out.println(node.getText()); }
      */
+
+    class ReplicationDescriptor {
+        private int position, replications;
+        private String type, name;
+
+        public ReplicationDescriptor (int position, String name, String type, int replications ) {
+            this.position = position;
+            this.replications = replications;
+            this.name = name;
+            this.type = type;
+         }
+
+         public int getReplications() {
+             return this.replications;
+         } 
+
+         public int getPosition() {
+             return this.position;
+         }
+
+         public String getType(){
+             return this.type;
+         }
+
+         public String getName() {
+             return this.name;
+         }
+
+         public void reduceReplications(int count) {
+             this.replications = this.replications - count;
+         }
+
+         public void reduceReplications() {
+             reduceReplications(1);
+         }
+         @Override
+         public String toString() {
+             return "Descriptor: Position " + this.position + " Name: " + this.name + " Replications: " + this.replications ; 
+         }
+    }
 }
