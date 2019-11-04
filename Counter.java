03ec05fe153edc7899dadc7d059d51a166fc5e7b
Markus Heene <markus.heene@gmail.com>
2019-11-04 16:09:31 +0100

    Logging integrated, start error handling

--- Counter.java
+++ Counter.java
@@ -1,17 +1,28 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
-@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast", "deprecation"})
-public class Counter {
-    public static void main(String[] args) throws Exception {
-	// create a CharStream that reads from standard input
-	ANTLRInputStream input = new ANTLRInputStream(System.in);
-	BUFRLexer lexer = new BUFRLexer(input);
-	CommonTokenStream tokens = new CommonTokenStream(lexer);
-	BUFRParser parser = new BUFRParser(tokens);
-	ParseTree tree = parser.template();
 
-	ParseTreeWalker walker = new ParseTreeWalker();
-	walker.walk(new CountReplicationDescriptor(), tree);
-	System.out.println();
-    }
+import java.util.List;
+import java.util.Iterator;
+
+@SuppressWarnings({ "all", "warnings", "unchecked", "unused", "cast", "deprecation" })
+public class Counter {
+	public static void main(String[] args) throws Exception {
+		// create a CharStream that reads from standard input
+		ANTLRInputStream input = new ANTLRInputStream(System.in);
+		BUFRLexer lexer = new BUFRLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		BUFRParser parser = new BUFRParser(tokens);
+		ParseTree tree = parser.template();
+
+		ParseTreeWalker walker = new ParseTreeWalker();
+		CountReplicationDescriptor crd = new CountReplicationDescriptor();
+		walker.walk(crd, tree);
+		List<String> errors = crd.getErrors();
+
+		Iterator<String> it = errors.listIterator();
+		while (it.hasNext()) {
+			System.err.println(it.next());
+		}
+
+	}
 }
