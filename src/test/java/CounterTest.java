5d82a0b17e3617dcdc0c6f6d0c42aba7aec9f58e
Markus Heene <markus.heene@gmail.com>
2019-11-20 12:41:35 +0100

    Cleanup test cases, build.gradle improved

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -1,11 +1,16 @@
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
-import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Iterator;
-import java.io.*;
+import java.util.List;
 
-import static org.junit.Assert.*;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.junit.Test;
 
 @SuppressWarnings({ "all", "warnings", "unchecked", "unused", "cast", "deprecation" })
@@ -30,9 +35,9 @@ public class CounterTest {
 
 	}
 
-	private ParserErrorMessages parseFile(FileInputStream fis) throws Exception {
+	private ParserErrorMessages parseFile(File p_file) throws Exception {
 		final InputStream original = System.in;
-
+		final FileInputStream fis = new FileInputStream(p_file);
 		System.setIn(fis);
 		String iterationErrorString = null;
 
@@ -57,6 +62,8 @@ public class CounterTest {
 		}
 
 		ParserErrorMessages pem = new ParserErrorMessages(iterationErrorString, verboseErrorListener.getErrors());
+
+		fis.close();
 
 		System.setIn(original);
 
@@ -218,13 +225,57 @@ public class CounterTest {
 				}
 
 				
-				assertNull("Iteration Errors should be null for file: " + fileEntry.getName(), iterationErrorString);
-				assertNotNull("Other Errors should not be null for file: " + fileEntry.getName(), verboseErrorListener.getErrors());
+				assertNotNull("Iteration Errors should not be null for file: " + fileEntry.getName(), iterationErrorString);
+				assertNull("Other Errors should be null for file: " + fileEntry.getName(), verboseErrorListener.getErrors());
 				fis.close();
 				System.setIn(original);
 
 			}
 		}
 
+	}
+
+	@Test
+	public void testInvalidReplications2() throws Exception {
+		final InputStream original = System.in;
+
+		final File validTestFileFolder = new File("build/classes/java/test/invalid/replications/both");
+		for (final File fileEntry : validTestFileFolder.listFiles()) {
+			if (fileEntry.isFile()) {
+				final FileInputStream fis = new FileInputStream(fileEntry);
+
+				System.setIn(fis);
+				String iterationErrorString = null;
+
+				ANTLRInputStream input = new ANTLRInputStream(System.in);
+				BUFRLexer lexer = new BUFRLexer(input);
+				CommonTokenStream tokens = new CommonTokenStream(lexer);
+				BUFRParser parser = new BUFRParser(tokens);
+				parser.removeErrorListeners();
+				VerboseListener verboseErrorListener = new VerboseListener();
+				parser.addErrorListener(verboseErrorListener);
+				ParseTree tree = parser.template();
+
+				ParseTreeWalker walker = new ParseTreeWalker();
+				CountReplicationDescriptor crd = new CountReplicationDescriptor();
+				walker.walk(crd, tree);
+				List<String> errors = crd.getErrors();
+
+				Iterator<String> it = errors.listIterator();
+				while (it.hasNext()) {
+					String errorText = it.next();
+					iterationErrorString = iterationErrorString + errorText;
+				}
+
+				assertNotNull("Iteration Errors should not be null for file: " + fileEntry.getName(), iterationErrorString);
+				assertNotNull("Other Errors should not be null for file: " + fileEntry.getName(), verboseErrorListener.getErrors());
+		
+				
+				fis.close();
+				System.setIn(original);
+
+			}
+		}
+	
 	}
 }
