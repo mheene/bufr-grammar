4bd536b8d93bdd385c4f0dfd4d65278803ec518a
Markus Heene <markus.heene@gmail.com>
2019-11-20 10:41:13 +0100

    gradle grun task implemented

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -2,13 +2,26 @@ import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Iterator;
 
 @SuppressWarnings({ "all", "warnings", "unchecked", "unused", "cast", "deprecation" })
 public class Counter {
 	public static void main(String[] args) throws Exception {
 		// create a CharStream that reads from standard input
-		ANTLRInputStream input = new ANTLRInputStream(System.in);
+		if (args.length == 0) {
+			System.err.println("Please provide a filename with descriptors");
+			System.exit(1);
+		} else if (args.length > 1) {
+			System.err.println("Please provide only one parameter");
+			System.exit(1);
+		}
+
+		File templateFile = new File(args[0]);
+		FileInputStream fis = new FileInputStream(templateFile);
+		ANTLRInputStream input = new ANTLRInputStream(fis);
+		//ANTLRInputStream input = new ANTLRInputStream(System.in);
 		BUFRLexer lexer = new BUFRLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		BUFRParser parser = new BUFRParser(tokens);
@@ -24,10 +37,10 @@ public class Counter {
 
 		Iterator<String> it = errors.listIterator();
 		while (it.hasNext()) {
-			System.err.println(it.next());
+			System.err.println("Errors iterator: " + it.next());
 		}
 
-		System.err.println(verboseErrorListener.getErrors());
+		System.err.println("Errors Listener: " + verboseErrorListener.getErrors());
 		
 
 	}
